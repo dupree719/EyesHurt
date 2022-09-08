349c4d41445ab62b4ce2c22f06c37dd234f6cdcd
dupree719 <isaiahdup7@gmail.com>
2022-09-08 01:01:09 -0400

    EyesHurt

---
+++ src/main/java/getLastIndex.java
@@ -0,0 +1,171 @@
+public class getLastIndex {
+//    //Q1
+//    public static int getLastIndex(String[] names) {
+//        return names.length - 1;
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        String[] names = {"bill", "sang", "jamal", "enrique"};
+//        System.out.println(getLastIndex(names));
+//    }
+//}
+//    Q2================
+//
+//    public static int getSecondToLastIndex(String[] names) {
+//        return names.length - 2;
+//    }
+//
+//    public static void main(String[] args) {
+//        String[] names = {"bill", "sang", "jamal", "enrique"};
+//        System.out.println(getSecondToLastIndex(names));
+//    }
+//}
+//
+//    Q3================
+//    public static int  getFirstElement(String[] names) {
+//        return names.length - 4;
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        String[] names = {"bill", "sang", "jamal", "enrique"};
+//        System.out.println((names[0]));
+//    }
+//}
+//
+//Q4=====================
+//    public static int  getLastElement(String[] names) {
+//        return names.length - 4;
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        String[] names = {"bill", "sang", "jamal", "enrique"};
+//        System.out.println((names[3]));
+//    }
+//}
+//
+//Q5==============
+//    public static int  getSecondToLastElement(String[] names) {
+//        return names.length - 4;
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        String[] names = {"bill", "sang", "jamal", "enrique"};
+//        System.out.println((names[2]));
+//    }
+//}
+//
+//    Q6==============
+//public static int getSum(int[] ints) {
+//    int sum = 0;
+//    int position = 0;
+//    while (position < ints.length - 1) {
+//        sum = sum + ints[position];
+//        position++;
+//    }
+//    return sum;
+//}
+//
+//    public static void main (String[]args){
+//        //declare the integer array
+//        int[] test = new int[8];
+//        test[0] = 3;
+//        test[1] = 3;
+//        test[2] = 3;
+//        test[3] = 4;
+//        test[4] = 5;
+//        test[5] = 6;
+//        test[6] = 7;
+//        test[7] = 0;
+//        System.out.println(getSum(test));
+//
+//            }
+//            //loop1 sum = 0 + 1
+//            //loop2 sum = 0 + 1 + 2
+//            //loop3 sum = 0 + 1 + 2 + 3
+//            //etc until total sum
+//
+//        }
+//
+//
+//Q7 ==========
+////
+//
+//    public static int getAverage(int[] ints) {
+//        int sum = 0;
+//        for (int value : ints) {
+//            sum += value;
+//        }
+//        return sum / ints.length;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] test = new int[8];
+//        test[0] = 10;
+//        test[1] = 20;
+//        test[2] = 30;
+//        test[3] = 40;
+//        test[4] = 50;
+//        test[5] = 60;
+//        test[6] = 50;
+//        test[7] = 50;
+//        System.out.println(getAverage(test));
+//    }
+//
+//}
+
+    //Q8=================
+//    public static String extractAllOddNumbers(int[] numbers) {
+//        int element = 0;
+//        int position =0;
+//        String oddNumbers = " ";
+//        return oddNumbers;
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        int a[]= {1,2,3,4,5,6};
+//
+//        System.out.println("Odd numbers.....");
+//        for(int i=0; i<a.length;i++)
+//        {
+//            if(a[i]%2!=0)
+//                System.out.println(a[i]);
+//
+//        }
+//
+//        }
+//    }
+
+//Q9===================
+
+//    public static String extractAllEvenNumbers(int[] numbers) {
+//        int element = 0;
+//        int position =0;
+//        String evenNumbers = " ";
+//        return evenNumbers;
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        int a[]= {1,2,3,4,5,6};
+//
+//        System.out.println("Even numbers.....");
+//        for(int i=0; i<a.length;i++)
+//        {
+//            if(a[i]%2==0)
+//                System.out.println(a[i]);
+//
+//        }
+//
+//    }
+//}
+
+    //Q10================
+//    public static boolean contains(String[] names, String element) {
+//        return false;
+//    }
+//}
